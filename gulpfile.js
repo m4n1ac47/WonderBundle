c08862bb9b42f173c4ecd7ef830b4db15a64bde9
m4n1ac47 <m4n1ac47@gmail.com>
2018-12-10 23:04:18 +0300

    minor update

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
-let gulp              = require('gulp'),
+let del               = require('del'),
+    gulp              = require('gulp'),
+    gutil             = require('gulp-util'),
+    rename            = require('gulp-rename'),
     notify            = require("gulp-notify"),
+    browserSync       = require('browser-sync'),
+    sourcemaps        = require('gulp-sourcemaps'),
     postcss           = require('gulp-postcss'),
     cssimport         = require('postcss-import'),
     nested            = require('postcss-nested'),
     short             = require('postcss-short'),
-    assets            = require('postcss-assets'),
-    rename            = require('gulp-rename'),
+    assets            = require('postcss-assets'), 
     cssnano           = require('cssnano'),
     autoprefixer      = require('autoprefixer'),
     postcssPresetEnv  = require('postcss-preset-env'),
@@ -25,7 +29,7 @@ let plugins = [
         postcssMixins,
         nested,
         assets({
-            loadPaths: ['app/assets/**/*'],
+            loadPaths: ['app/assets/fonts/**/*', 'app/assets/img/', 'app/assets/img/**/*'],
             relativeTo: 'app/assets/css/'
         }),
         short,
@@ -39,12 +43,35 @@ let plugins = [
 
 gulp.task('css', function () {
     return gulp.src('./app/assets/postcss/*.css')
+        .pipe(gutil.env.type === 'production' ? gutil.noop() : sourcemaps.init())
         .pipe(postcss(plugins).on("error", notify.onError()))
         .pipe(rename('styles.min.css'))
-        .pipe(gulp.dest('./app/assets/css/'));
+        .pipe(gutil.env.type === 'production' ? gutil.noop() : sourcemaps.write('.'))
+        .pipe(gulp.dest('./app/assets/css/'))
+        .pipe(browserSync.stream())
 });
 
+gulp.task('default', ['watch']); // gulp --type production
 
-gulp.task('watch', ['css'], function() {
+gulp.task('clean', function() {
+    return del.sync('dist');
+});
+
+gulp.task('browser-sync', function() {
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        // proxy: 'gulp',
+        notify: false,
+        open: true,
+        // online: false, // Work Offline Without Internet Connection
+        // tunnel: true, tunnel: "projectname", // Demonstration page: http://projectname.localtunnel.me
+    })
+});
+
+gulp.task('watch', ['css', 'browser-sync'], function() {
     gulp.watch('app/assets/postcss/**/*.css', ['css']);
+    gulp.watch('app/**/*.php', browserSync.reload);
+    gulp.watch('app/*.html', browserSync.reload);
 });
